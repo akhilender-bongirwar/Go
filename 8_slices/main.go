c857196ef812d1167d7bd99115b2616cce43467c
Akhil <akhilenderb9@gmail.com>
2023-08-25 16:20:46 +0530

    learning slices

---
+++ 8_slices/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println("I am slice. You love to eat me")
+	var fruitList = []string{"Hi"}
+	fmt.Printf("the fruitlist is of %T ", fruitList)
+	fmt.Println(fruitList)
+	fmt.Println(len(fruitList))
+
+	fruitList = append(fruitList, "Bro do ", "you think this is ", "a fruitList")
+	fmt.Println(len(fruitList))
+	fmt.Println(fruitList)
+
+	fruitList = append(fruitList[:1])
+	fmt.Println(len(fruitList))
+	fmt.Println(fruitList)
+
+	marks := make([]int, 2)
+	marks[0] = 100
+	marks[1] = 78
+	fmt.Println(marks)
+	marks = append(marks, 67, 39, 10)
+	fmt.Println(marks)
+	sort.Ints(marks)
+	fmt.Println(sort.IntsAreSorted(marks))
+	fmt.Println(marks)
+}
