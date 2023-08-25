d8f8eada11900af372ed43a0fc4bbb86ce98207f
Akhil <akhilenderb9@gmail.com>
2023-08-25 18:34:49 +0530

    Switch case
    Here you dont need to put break at the end of every case just like in java we use -> to not write break but to execute the exact down case then just write fallthrough

--- 12_switch/main.go
+++ 12_switch/main.go
@@ -10,4 +10,18 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 	diceNum := rand.Intn(6) + 1
 	fmt.Println("Value of dice is", diceNum)
+
+	switch diceNum {
+	case 1:
+		fmt.Println("Dice value is 1 and you move 1")
+		fallthrough
+	case 2:
+		fmt.Println("Dice value is 2 and now you move 2")
+		fallthrough
+	case 3:
+		fmt.Println("Dice val is 3 and so on ....")
+		fallthrough
+	default:
+		fmt.Println("Handle it yourselves bro!!")
+	}
 }
