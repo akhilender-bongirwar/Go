20f1a34179ecf6f3ba0d08cc3bf632e62c714500
Akhil <akhilenderb9@gmail.com>
2023-08-25 21:58:28 +0530

    Methods
    Nothing new just getters and may be I will learn setters also

--- 15_methods/main.go
+++ 15_methods/main.go
@@ -6,7 +6,10 @@ func main() {
 	fmt.Println("These are the structure in go")
 	akhil := User{"Akhil", "I am cool", true, 1}
 	fmt.Println(akhil)
-	fmt.Printf("akhil details are %+v", akhil)
+	fmt.Printf("akhil details are %+v \n", akhil)
+	akhil.GetStatus()
+	akhil.NewMsg()
+	fmt.Println("Message is ", akhil.Message)
 }
 
 type User struct {
@@ -14,4 +17,12 @@ type User struct {
 	Message string
 	Status  bool
 	Count   int
+}
+
+func (u User) GetStatus() {
+	fmt.Println("Is user active:", u.Status)
+}
+func (u User) NewMsg() {
+	u.Message = "Bro wtf"
+	fmt.Println(u.Message)
 }
